e4fed3c1063140791f4a3315db7661ce006ccdb5
berke6565 <97334335+berke6565@users.noreply.github.com>
2022-01-13 20:12:04 +0300

    Moderation

---
+++ settings.js
@@ -0,0 +1,61 @@
+module.exports = {
+    bot: {
+        botOwner: ["883423490013814794"],
+        botToken: "OTI5NTA2MDg3NDQ1NjEwNTI3.YdoUAA.8E34K6Ix8r1Uxm4n0PksUwvHmoY",
+        botPrefix: ".",
+        botStatus: "❃ Utopia 💛",
+        botVoice: "930134917373894677",
+        mongoURL: "mongodb+srv://hadesdatabase:database123@hadeslog.6rijw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    },
+    
+
+    roles: {
+        muteStaff: ["930134803330760765"],
+        vmuteStaff: ["930134803330760765"],
+        jailStaff: ["930134802445774868"],
+        banStaff: ["930134801275576381"],
+        muteRole: "930134834641272852",
+        vMuteRole: "930134834641272852",
+        jailRole: "930134833823367238",
+        unregisterRoles: ["930134849593962576"],
+        tagRole: "930134839418572840",
+        vipRole: "930134841209552916",
+        boosterRole: "929444524651061269",
+        enAltYt: "930134796947042415",
+        katıldı: "931232804216393848",
+        yasaklıTag: "930134835618512967"
+    },
+
+    channels: {
+        genelChat: "930134946801156096",
+        tagLog: "931068395502325771",
+        muteLog: "931068395502325771",
+        vMuteLog: "931068395502325771",
+        jailLog: "931068395502325771",
+        banLog: "931068395502325771",
+        messageLog: "931068292532154378",
+        meeting: "",
+        rolLog: "931068395502325771",
+        voiceLog: "931068292532154378",
+        yasaklıtagLog: "931068395502325771",
+        pubCategoryID: ["930134890488426547"],
+
+    },
+
+    guild: {
+        guildID: "929435235307905035",
+        tagges: ["❃"],
+        nameTag: "❃",
+        defaultTag: "❃",
+        banLimit: 3
+
+    },
+
+    emojis: {
+        yes: "<a:utopia_evet:929633145953845248>",
+        no: "<a:utopia_hyr:929633092203868210>",
+        on: "<a:utopia_evet:929633145953845248>",
+        off: "<a:utopia_hyr:929633092203868210>"
+    }
+
+};
